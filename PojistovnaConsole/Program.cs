49f6b5068562f854ffa4e87b25bb73b7b82cc226
venca1998 <petelikv@seznam.cz>
2024-01-02 23:38:19 +0100

    First Committ

---
+++ PojistovnaConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PojistovnaConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawMenu.Header("Federal Example Republic Digital Property Insurance Company");
+            DrawMenu.ExecutionMenu();
+        }
+
+        
+    }
+}
